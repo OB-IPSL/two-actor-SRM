fe591640744695e7e3a4a9201ad106b833cfdde2
Jerome Bureau <jerome.bureau@latmos.ipsl.fr>
2025-05-16 11:19:48 +0200

    avant interversion tableaux 4D

--- carte-kikp.py
+++ carte-kikp.py
@@ -495,17 +495,17 @@ iki=1
 ikp=1
 
 iep=2
-ecrit1d(fo,"g_SRM_nh","f8",("t"),g_SRM_nh[iep,:,ikp,iki])
-ecrit1d(fo,"g_SRM_sh","f8",("t"),g_SRM_sh[iep,:,ikp,iki])
-ecrit1d(fo,"T_SRM_nh","f8",("t"),T_SRM_nh[iep,:,ikp,iki])
-ecrit1d(fo,"T_SRM_sh","f8",("t"),T_SRM_sh[iep,:,ikp,iki])
-ecrit1d(fo,"monsoon_SRM","f8",("t"),monsoon_SRM[iep,:,ikp,iki])
+#ecrit1d(fo,"g_SRM_nh","f8",("t"),g_SRM_nh[iep,:,ikp,iki])
+#ecrit1d(fo,"g_SRM_sh","f8",("t"),g_SRM_sh[iep,:,ikp,iki])
+#ecrit1d(fo,"T_SRM_nh","f8",("t"),T_SRM_nh[iep,:,ikp,iki])
+#ecrit1d(fo,"T_SRM_sh","f8",("t"),T_SRM_sh[iep,:,ikp,iki])
+#ecrit1d(fo,"monsoon_SRM","f8",("t"),monsoon_SRM[iep,:,ikp,iki])
 
-#ecrit3d(fo,"g_SRM_nh","f8",("t","kp","ki"),g_SRM_nh)
-#ecrit3d(fo,"g_SRM_sh","f8",("t","kp","ki"),g_SRM_sh)
-#ecrit3d(fo,"T_SRM_nh","f8",("t","kp","ki"),T_SRM_nh)
-#ecrit3d(fo,"T_SRM_sh","f8",("t","kp","ki"),T_SRM_sh)
-#ecrit3d(fo,"monsoon_SRM","f8",("t","kp","ki"),monsoon_SRM)
+ecrit3d(fo,"g_SRM_nh","f8",("t","kp","ki"),g_SRM_nh)
+ecrit3d(fo,"g_SRM_sh","f8",("t","kp","ki"),g_SRM_sh)
+ecrit3d(fo,"T_SRM_nh","f8",("t","kp","ki"),T_SRM_nh)
+ecrit3d(fo,"T_SRM_sh","f8",("t","kp","ki"),T_SRM_sh)
+ecrit3d(fo,"monsoon_SRM","f8",("t","kp","ki"),monsoon_SRM)
 
 
 
