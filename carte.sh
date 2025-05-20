670f4719ca9fd61a1bb520085da8bc70f594ced3
Jerome Bureau <jerome.bureau@latmos.ipsl.fr>
2025-05-20 17:29:02 +0200

    .

--- carte.sh
+++ carte.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+
+echo "nh"
 python3 carte-kikp.py conf-1a-kikp-nh.py
+echo "gl"
+python3 carte-kikp.py conf-1a-kikp-gl.py
+echo "sh"
+python3 carte-kikp.py conf-1a-kikp-sh.py
+echo "nh bruit nul"
+python3 carte-kikp.py conf-1a-kikp-gl-bruitnul.py
+echo "gl bruit nul"
+python3 carte-kikp.py conf-1a-kikp-nh-bruitnul.py
+echo "sh bruit nul"
+python3 carte-kikp.py conf-1a-kikp-sh-bruitnul.py
