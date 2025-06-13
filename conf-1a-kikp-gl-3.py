7d29303e8308aa50fe4dd8aeda5db1a7cd4e3a31
Jerome Bureau <jerome.bureau@latmos.ipsl.fr>
2025-06-13 12:35:52 +0200

    .

--- conf-1a-kikp-gl-3.py
+++ conf-1a-kikp-gl-3.py
@@ -72,7 +72,7 @@ tau_nh_sh_lower=20.
 noise_type='red'
 noise_T=0.15       #--in K
 noise_monsoon=0.   #--in % change
-
+./
 
 # noisefilei: file with noise input (temperatures and moonson)
 #             takes precedence over all noise parameters.
